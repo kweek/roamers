1a1398ef1d35f1f3cbedf09ffa1768042da8b3ae
kweek <kdweekley@gmail.com>
2017-03-25 11:51:33 -0600

    basic router has been created

--- public/app.js
+++ public/app.js
@@ -1,7 +1,29 @@
 angular.module('roamers', ['ui.router'])
   .config(function ($stateProvider, $urlRouterProvider){
       $stateProvider
-          .state('home')
+          .state('home', {
+            url: '/',
+            templateUrl: './public/home/home.html',
+          })
+          .state('howItWorks', {
+            url: '/howItWorks',
+            templateUrl: './public/howItWorks/howItWorks.html',
+            controller: 'hitCtrl',
+          })
+          .state('itinerary', {
+            url: '/itinerary',
+            templateUrl: './public/itinerary/itinerary.html',
+            controller: 'itinCtrl',
+          })
+          .state('followUs', {
+            url: '/followUs',
+            templateUrl: './public/followUs/followUs.html',
+            controller: 'followUsCtrl',
+          })
+          .state('apply', {
+            url: 'apply',
+            templateUrl: './public/apply/apply.html',
+            controller: 'applyCtrl',
+          })
 
-
-  })
+  });
